bde355e44b685c768665c18ff7b533da3f1a0e83
Lunber <891504539@qq.com>
2016-09-07 18:05:32 +0800

    20160907

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ var Router = require('koa-router');
 var json = require('koa-json');
 var cors = require('koa-cors');
 var co = require('co');
+var serve = require('koa-static')
+
 
 app.use(json())
 app.use(cors());
+
+var main_router = new Router({})
 
 var router = new Router({
     prefix: '/api'
@@ -13,12 +17,18 @@ var router = new Router({
 
 router.get('/', function *(next) {
     this.response.body = 'Hello World!';
-});
-
-router.get('/test', function* (next) {
+})
+    .get('/test', function* (next) {
     this.response.body = {msg: 'hello json'}
 })
 
+main_router.get('/',function* (next) {
+    this.response.body = 'Hello main_router'
+})
+
 app.use(router.routes());
+app.use(main_router.routes())
+// app.use(serve('backhtml'))
+// app.use(serve('dist'))
 
 app.listen(8888);
