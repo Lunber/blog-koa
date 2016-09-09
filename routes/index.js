70caa6a3ab8ff5918898c897c0f368e7b0e3c9db
Lunber <891504539@qq.com>
2016-09-09 18:26:17 +0800

    api router session

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,55 @@
 /**
  * Created by lwb on 2016/9/2.
  */
-model.exports = function (router) {
-    router.get('/', function *(next) {
-        this.response.body = 'Hello World!';
-    });
+var Router = require('koa-router');
+var views = require('co-views')
+var koaBody = require('koa-body')();
+var session = require('koa-session');
 
-    router.get('/test', function* (next) {
-        this.response.body = {msg: 'hello json'}
+var main_router = new Router({})
+var api_router = new Router({
+    prefix: '/api'
+});
+var render = views('backhtml' , {
+    map: { html: 'swig' }
+})
+
+module.exports = function(app) {
+    app.keys = ['some secret hurr'];
+    app.use(session(app));
+    api_router.get('/', function*(next) {
+            this.response.body = 'Hello World!';
+        })
+        .get('/test', function*(next) {
+            this.response.body = {
+                msg: 'hello json'
+            }
+        })
+        .post('/admin' ,koaBody, function* (next) {
+            console.log(this.request.body.pwd)
+            console.log(this.session)
+            if (this.request.body.pwd == 123456){
+                this.response.body = {
+                    msg: 'success',
+                    code:'1'
+                }
+                this.session.login = "login"
+                console.log(this.session.login)
+                console.log(this.session)
+            }else {
+                this.response.body = {
+                    msg: 'false',
+                    code:'0'
+                }
+            }
+        })
+
+    main_router.get('/admin', function*(next) {
+        this.body = yield render('index')
+    }).get('/admin/home',function* (next) {
+        this.body = yield render('admin_home')
     })
+
+    app.use(api_router.routes());
+    app.use(main_router.routes())
 }
--- server.js
+++ server.js
@@ -4,53 +4,19 @@ var json = require('koa-json');
 var cors = require('koa-cors');
 var co = require('co');
 var serve = require('koa-static')
-var ejs = require('ejs')
-var views = require('co-views')
-// var render = require('co-ejs')
-// var path = require('path');
+var session = require('koa-session');
 
+var routes = require('./routes/index')
 
-app.use(json())
+app.use(json());
 app.use(cors());
-// app.use(render(app, {
-//   root: path.join(__dirname, 'backhtml'),
-//   layout: 'layout',
-//   viewExt: 'ejs',
-//   cache: true,
-//   debug: true
-// }));
-var render = views('backhtml' , {
-   map: { html: 'swig' }
-})
-// render(app, {
-//   root: path.join(__dirname, 'backhtml'),
-//   layout: '__layout',
-//   viewExt: 'ejs',
-//   cache: false,
-//   debug: true
-// });
+// app.use(session(app));
+app.keys = ['some secret hurr'];
+app.use(session(app));
 
-var main_router = new Router({})
+routes(app);
 
-var api_router = new Router({
-    prefix: '/api'
-});
-
-api_router.get('/', function *(next) {
-    this.response.body = 'Hello World!';
-})
-    .get('/test', function* (next) {
-    this.response.body = {msg: 'hello json'}
-})
-
-main_router.get('/admin',function* (next) {
-  this.body = yield render('index')
-  // yield this.render('index');
-})
-
-app.use(api_router.routes());
-app.use(main_router.routes())
-// app.use(serve('backhtml'))
 app.use(serve('dist'))
 
 app.listen(8888);
+console.log("listen port 8888")
