ef6367720f0e385f6f93bc99bdd8ae5210d754fc
Lunber <891504539@qq.com>
2016-09-12 14:20:48 +0800

    完成后台登入登出

--- routes/index.js
+++ routes/index.js
@@ -10,46 +10,61 @@ var main_router = new Router({})
 var api_router = new Router({
     prefix: '/api'
 });
-var render = views('backhtml' , {
-    map: { html: 'swig' }
+var render = views('backhtml', {
+    map: {html: 'swig'}
 })
 
-module.exports = function(app) {
-    app.keys = ['some secret hurr'];
+module.exports = function (app) {
+    app.keys = ['Lunber cookie key'];
     app.use(session(app));
     api_router.get('/', function*(next) {
-            this.response.body = 'Hello World!';
-        })
+        this.response.body = 'Hello World!';
+    })
         .get('/test', function*(next) {
             this.response.body = {
                 msg: 'hello json'
             }
         })
-        .post('/admin' ,koaBody, function* (next) {
+        .post('/admin', koaBody, function*(next) {
             console.log(this.request.body.pwd)
             console.log(this.session)
-            if (this.request.body.pwd == 123456){
+            if (this.request.body.pwd == 123456) {
                 this.response.body = {
                     msg: 'success',
-                    code:'1'
+                    code: '1'
                 }
                 this.session.login = "login"
                 console.log(this.session.login)
                 console.log(this.session)
-            }else {
+            } else {
                 this.response.body = {
                     msg: 'false',
-                    code:'0'
+                    code: '0'
                 }
             }
+        })
+        .get('/signout',function* (next) {
+            this.response.body = {
+                msg: 'success',
+                code: '1'
+            }
+            this.session.login = ""
         })
 
     main_router.get('/admin', function*(next) {
         this.body = yield render('index')
-    }).get('/admin/home',function* (next) {
-        this.body = yield render('admin_home')
-    })
+    }).get('/admin/home', checkLogin)
+        .get('/admin/home', function*(next) {
+            this.body = yield render('admin_home')
+        })
 
     app.use(api_router.routes());
-    app.use(main_router.routes())
+    app.use(main_router.routes());
+}
+
+var checkLogin = function*(next) {
+    if (!this.session.login) {
+        return this.redirect('/admin')
+    }
+    yield next
 }
