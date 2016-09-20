b4478708d77b97cfdc5513135b370359ca1cb6b4
Lunber <891504539@qq.com>
2016-09-20 18:32:21 +0800

    mongoose 模型

--- model/mongoose.js
+++ model/mongoose.js
@@ -5,6 +5,10 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/blog');
-var db = mongoose.connection.on('error', console.error.bind(console, '连接数据库失败'));
-
+var db = mongoose.connection
+db.on('error', console.error.bind(console, '连接数据库失败'));
+var postSchema = mongoose.Schema({
+    post:String
+})
+var post = mongoose.
 module.exports = db
